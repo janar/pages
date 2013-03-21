ae30fa9d3d4b8ceaacbdf1275901ebaee90b34d6
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2013-03-21 11:39:45 +0530

    Add category name and link to category on pages

--- views/page/view.blade.php
+++ views/page/view.blade.php
@@ -1,4 +1,17 @@
-<h1 id="page-title">{{ $page->name }}</h1>
+<h1 id="page-title">{{ $page->name }}
+    @if ($page->category !== null)
+    <small>{{ $page->category->name }}</small></h1>
+    @else
+</h1>
+    @endif
+
+@if ($page->category !== null)
+<div class="margin-top margin-bottom-xxlarge">
+    <a class="btn btn-small" href="{{ url('category/' . Pages\Category::full_slug($page->category->id)[0]) }}">
+        <i class="icon-chevron-left"></i> Back to {{ $page->category->name }}
+    </a>
+</div>
+@endif
 
 <div class="content">
     {{ $page->body }}
