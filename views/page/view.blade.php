89890f1cc97cca7d2cc10b64d23eaf2efcb642ca
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2013-03-21 11:54:39 +0530

    Fix error printing category full_slug in PHP 5.3
    PHP 5.3 was not able to dynamically grab the full_slug and return the
    first item of the array. Fix it by first loading result into a variable
    and printing the first item of the array.

--- views/page/view.blade.php
+++ views/page/view.blade.php
@@ -6,8 +6,11 @@
     @endif
 
 @if ($page->category !== null)
+
+<?php $category_full_slug = Pages\Category::full_slug($page->category->id); ?>
+
 <div class="margin-top margin-bottom-xxlarge">
-    <a class="btn btn-small" href="{{ url('category/' . Pages\Category::full_slug($page->category->id)[0]) }}">
+    <a class="btn btn-small" href="{{ url('category/' . $category_full_slug[0]) }}">
         <i class="icon-chevron-left"></i> Back to {{ $page->category->name }}
     </a>
 </div>
