acd3b34fd9047dee108c7afd68e38862af225f85
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2013-09-10 22:15:21 +0530

    Add class for category pages and published dates
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- views/category/list.blade.php
+++ views/category/list.blade.php
@@ -1,61 +1,63 @@
-<h1 id="page-title">{{ $category->name }}</h1>
+<div class="category-{{ $category->slug }}">
+    <h1 id="page-title">{{ $category->name }}</h1>
 
-@if ($category->parent !== null)
-<a class="btn btn-small btn-inverse" href="{{ substr(URL::current(), 0, strrpos(URL::current(), '/')) }}">
-    <i class="icon-chevron-left icon-white"></i> Back to {{ $category->parent->name }}
-</a>
-@endif
+    @if ($category->parent !== null)
+        <a class="btn btn-small btn-inverse" href="{{ substr(URL::current(), 0, strrpos(URL::current(), '/')) }}">
+            <i class="icon-chevron-left icon-white"></i> Back to {{ $category->parent->name }}
+        </a>
+    @endif
 
-@if (count($children))
-<div class="category-children divider-bottom">
-    <h2>Browse {{ $category->name }}</h2>
-    <ul class="thumbnails">
-        @foreach ($children as $child)
-        <li class="span3">
-            <div class="thumbnail">
-                <h3 class="text-center">
-                    <a href="{{ URL::current() . '/' . $child->slug }}">
-                        {{ $child->name }}
-                    </a>
-                </h3>
-                <a href="{{ URL::current() . '/' . $child->slug }}" class="btn btn-block btn-small">
-                    <i class="icon-chevron-right"></i> View
-                </a>
-            </div>
-        </li>
-        @endforeach
-    </ul>
-</div>
-@endif
+    @if (count($children))
+        <div class="category-children divider-bottom">
+            <h2>Browse {{ $category->name }}</h2>
+            <ul class="thumbnails">
+                @foreach ($children as $child)
+                    <li class="span3">
+                        <div class="thumbnail">
+                            <h3 class="text-center">
+                                <a href="{{ URL::current() . '/' . $child->slug }}">
+                                    {{ $child->name }}
+                                </a>
+                            </h3>
+                            <a href="{{ URL::current() . '/' . $child->slug }}" class="btn btn-block btn-small">
+                                <i class="icon-chevron-right"></i> View
+                            </a>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
-<div class="category-pages">
-    @foreach ($pages->results as $page)
-    <div class="category-page divider-bottom">
+    <div class="category-pages">
+        @foreach ($pages->results as $page)
+            <div class="category-page divider-bottom">
         <?php $full_slug = substr(URI::current(), strpos(URI::current(), '/'));
         $full_slug = rtrim($full_slug, '/') . '/' . $page->slug;
         ?>
-        <h3><a href="{{ url($full_slug) }}">{{ $page->name }}</a></h3>
-        <p class="teaser divider-left">
-            {{ Str::words(strip_tags($page->body), 60) . '&hellip;' }}
-        </p>
-        <div class="row">
-            <div class="span6 text-right">
-                <small>Published: {{ date('jS M, Y', strtotime($page->created_at)) }}</small>
+                <h3><a href="{{ url($full_slug) }}">{{ $page->name }}</a></h3>
+                <p class="teaser divider-left">
+                    {{ Str::words(strip_tags($page->body), 60) . '&hellip;' }}
+                </p>
+                <div class="row">
+                    <div class="span6 text-right published-date">
+                        <small>Published: {{ date('jS M, Y', strtotime($page->created_at)) }}</small>
+                    </div>
+                    <div class="span2 offset1">
+                        <a class="btn btn-small" href={{ url($full_slug) }}>
+                            <i class="icon-chevron-right"></i> Read More
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div class="span2 offset1">
-                <a class="btn btn-small" href={{ url($full_slug) }}>
-                    <i class="icon-chevron-right"></i> Read More
-                </a>
-            </div>
-        </div>
+        @endforeach
     </div>
-    @endforeach
-</div>
 
-@if (count($pages->results) === 0 and count($children) === 0)
-<p class="muted">Nothing has been published in {{ $category->name }} yet.</p>
-@endif
+    @if (count($pages->results) === 0 and count($children) === 0)
+        <p class="muted">Nothing has been published in {{ $category->name }} yet.</p>
+    @endif
 
-<div class="pagination">
-    {{ $pages->links() }}
+    <div class="pagination">
+        {{ $pages->links() }}
+    </div>
 </div>
